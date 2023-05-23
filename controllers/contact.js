7ee023526cfa1e33f254e50f6890fa96a675ef38
Vector-ops <sumith2827@gmail.com>
2023-05-23 21:46:58 +0530

    refact: replace async wrapper with async handler

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,47 +1,48 @@
+const asyncHandler = require("express-async-handler");
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getAllContacts = (req, res) => {
+const getAllContacts = asyncHandler((req, res) => {
   res.status(200).json({
     contact: "all contacts",
   });
-};
+});
 
 //@desc Get one contacts
 //@route GET /api/contacts:id
 //@access public
-const getOneContact = (req, res) => {
+const getOneContact = asyncHandler((req, res) => {
   res.status(200).json({
     contact: "one contact",
   });
-};
+});
 
 //@desc create contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler((req, res) => {
   res.status(201).json({
     contact: "created",
   });
-};
+});
 
 //@desc Delete contacts
 //@route DELETE /api/contacts:id
 //@access public
-const deleteContacts = (req, res) => {
+const deleteContacts = asyncHandler((req, res) => {
   res.status(200).json({
     contact: "deleted",
   });
-};
+});
 
 //@desc Update contacts
 //@route PATCH /api/contacts:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler((req, res) => {
   res.status(203).json({
     contact: "updated",
   });
-};
+});
 
 module.exports = {
   getAllContacts,
