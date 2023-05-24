2193e645769c9ed65af5b8dd19b6387eba9036ad
Vector-ops <sumith2827@gmail.com>
2023-05-24 11:31:35 +0530

    refact: fix schema error

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
-const contactSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide contact name"],
+const contactSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide contact name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide email"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Please provide phone number"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please provide email"],
-  },
-  phone: {
-    type: String,
-    required: [true, "Please provide phone number"],
-  },
-  timestamp: true,
-});
+  {
+    timestamp: true,
+  }
+);
 
 module.exports = mongoose.model("Contact", contactSchema);
