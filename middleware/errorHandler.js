562d3a4d3f25434b41b9529606c4dacdbf1d7f41
Vector-ops <sumith2827@gmail.com>
2023-05-24 12:12:35 +0530

    refact: change error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,47 +1,12 @@
-const { constants } = require("../constants");
-
 const errorHandler = (err, req, res, next) => {
-  const status = res.status ? res.status : 500;
-  switch (status) {
-    case constants.VALIDATON_ERROR:
-      res.json({
-        title: "Validation Error",
-        message: err.message,
-        stacktrace: err.stack,
-      });
-      break;
-    case constants.NOT_FOUND:
-      res.json({
-        title: "Not Found",
-        message: err.message,
-        stacktrace: err.stack,
-      });
-      break;
-    case constants.UNAUTHORIZED:
-      res.json({
-        title: "Unauthorized",
-        message: err.message,
-        stacktrace: err.stack,
-      });
-      break;
-    case constants.FORBIDDEN:
-      res.json({
-        title: "Forbidden",
-        message: err.message,
-        stacktrace: err.stack,
-      });
-      break;
-    case constants.SERVER_ERROR:
-      res.json({
-        title: "Server Error",
-        message: err.message,
-        stacktrace: err.stack,
-      });
-      break;
-    default:
-      console.log("No error");
-      break;
-  }
+  const status = err.status ? err.status : 500;
+  res.status(status);
+  res.json({
+    error: {
+      status: status,
+      message: err.message,
+    },
+  });
 };
 
 module.exports = errorHandler;
