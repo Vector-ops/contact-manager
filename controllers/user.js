46042fbf41238868d655b50a844abeb0bafa2aea
Vector-ops <sumith2827@gmail.com>
2023-06-03 18:02:05 +0530

    fix: add authentication controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,26 +1,62 @@
 const asyncHandler = require("express-async-handler");
 const { authSchema } = require("../helpers/validationSchema");
 const createHttpError = require("http-errors");
+const User = require("../models/userModel");
+const { signAccessToken, verifyAccessToken } = require("../helpers/jwtHelper");
+const { valid } = require("joi");
+
 //@desc Register a user
 //@route POST /api/user/register
 //@access public
-const userRegister = asyncHandler(async (req, res) => {
+const userRegister = asyncHandler(async (req, res, next) => {
   const validation = await authSchema.validateAsync(req.body);
-  res.json({ msg: "register" });
+  const userExists = await User.findOne({ email: validation.email });
+  if (userExists) throw createHttpError.BadRequest("User already exists");
+  const user = await User.create(validation);
+  if (user) {
+    const accessToken = await signAccessToken(
+      user.id,
+      user.email,
+      user.username
+    );
+    return res.status(201).json({ accessToken: accessToken });
+  }
+  return next(createHttpError.Unauthorized("User data not valid"));
 });
 
 //@desc User login
 //@route POST /api/user/login
 //@access public
 const userLogin = asyncHandler(async (req, res) => {
-  res.json({ msg: "login" });
+  const validation = await authSchema.validateAsync(req.body);
+  const user = await User.findOne({
+    username: validation.username,
+    email: validation.email,
+  });
+  if (!user) {
+    throw createHttpError.Unauthorized("Invalid Credentials");
+  }
+  const isMatch = await user.isValidPassword(validation.password);
+  console.log(isMatch);
+  if (!isMatch) {
+    throw createHttpError.Unauthorized("Invalid credentials");
+  } else {
+    const accessToken = await signAccessToken(
+      user.id,
+      user.email,
+      user.username
+    );
+    return res.status(200).json({ accessToken: accessToken });
+  }
 });
 
 //@desc View user profile
-//@route POST /api/user/profile
+//@route GET /api/user/profile
 //@access private
-const userProfile = asyncHandler(async (req, res) => {
-  res.json({ msg: "profile" });
+const userProfile = asyncHandler(async (req, res, next) => {
+  const user = req.user;
+
+  res.json({ user });
 });
 
 //@desc User logout
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,11 +5,12 @@ const {
   userProfile,
   userLogout,
 } = require("../controllers/user");
+const { verifyAccessToken } = require("../helpers/jwtHelper");
 const router = express.Router();
 
 router.route("/register").post(userRegister);
 router.route("/login").post(userLogin);
-router.route("/profile").post(userProfile);
+router.route("/profile").get(verifyAccessToken, userProfile);
 router.route("/logout").get(userLogout);
 
 module.exports = router;
